4b5b414f121fddb81c129344c9a593dd0eee200d
xxxDKGxxx <dominikzielinski678@gmail.com>
2026-03-10 21:25:23 +0100

    ImGui support for the project

--- Source/GUI/MainComponent.cpp
+++ Source/GUI/MainComponent.cpp
@@ -1,8 +1,11 @@
 #include "MainComponent.h"
+#include "imgui.h"
+#include "implot.h"
 #include "juce_audio_basics/juce_audio_basics.h"
 #include "juce_audio_formats/juce_audio_formats.h"
 #include "juce_core/juce_core.h"
 #include "juce_gui_basics/juce_gui_basics.h"
+#include <cstddef>
 #include <memory>
 
 //==============================================================================
@@ -19,19 +22,19 @@ MainComponent::MainComponent() {
   addAndMakeVisible(*this->pMenuBarComponent);
 
   audioFormatManager.registerBasicFormats();
+
+  glctx.setOpenGLVersionRequired(juce::OpenGLContext::openGL3_2);
+  glctx.setRenderer(this);
+  glctx.attachTo(*this);
+  glctx.setContinuousRepainting(true);
 }
 
-MainComponent::~MainComponent() {}
+MainComponent::~MainComponent() { glctx.detach(); }
 
 //==============================================================================
 void MainComponent::paint(juce::Graphics &g) {
   // (Our component is opaque, so we must completely fill the background with a
   // solid colour)
-  g.fillAll(
-      getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
-
-  g.setFont(juce::FontOptions(16.0f));
-  g.setColour(juce::Colours::white);
 }
 
 void MainComponent::resized() {
@@ -91,4 +94,64 @@ void MainComponent::setMenuBarBounds() {
 
   pMenuBarComponent->setBounds(
       area.removeFromTop(getLookAndFeel().getDefaultMenuBarHeight()));
+
+  imguiOffsetY = getLookAndFeel().getDefaultMenuBarHeight();
 }
+
+void MainComponent::newOpenGLContextCreated() {
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImPlot::CreateContext();
+  ImGui_ImplJuce_Init(*this, glctx);
+  ImGui_ImplOpenGL3_Init();
+};
+
+void MainComponent::renderOpenGL() {
+
+  ImGui_ImplOpenGL3_NewFrame();
+  ImGui_ImplJuce_NewFrame();
+  ImGui::NewFrame();
+
+  auto commonFlags =
+      ImGuiWindowFlags_NoMove | ImGuiWindowFlags_AlwaysAutoResize |
+      ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse |
+      ImGuiWindowFlags_NoBringToFrontOnFocus |
+      ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNavFocus;
+
+  ImGui::SetNextWindowPos(ImVec2(0, 0 + imguiOffsetY));
+  ImGui::SetNextWindowSize(ImVec2(0.7 * getWidth(), 0.3 * getHeight()));
+
+  ImGui::Begin("Audio waveform", NULL, commonFlags);
+
+  if (pAudioModel != nullptr && pAudioModel->getLengthInSamples() > 0 &&
+      ImPlot::BeginPlot("Przebieg Audio",
+                        ImVec2(0.7 * getWidth(), 0.25 * getHeight()))) {
+    ImPlot::PlotLine("", pAudioModel->getAudioBuffer().getReadPointer(0),
+                     pAudioModel->getLengthInSamples());
+    ImPlot::EndPlot();
+  }
+
+  ImGui::End();
+
+  ImGui::SetNextWindowPos(ImVec2(0.7 * getWidth(), 0 + imguiOffsetY));
+  ImGui::SetNextWindowSize(ImVec2(0.3 * getWidth(), 0.3 * getHeight()));
+
+  ImGui::Begin("Headers", NULL, commonFlags);
+  ImGui::End();
+
+  ImGui::Render();
+
+  // background begin
+  juce::gl::glClearColor(0, 0, 0, 1);
+  juce::gl::glClear(juce::gl::GL_COLOR_BUFFER_BIT);
+  // background end
+
+  ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+};
+
+void MainComponent::openGLContextClosing() {
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplJuce_Shutdown();
+  ImGui::DestroyContext();
+  ImPlot::DestroyContext();
+};
