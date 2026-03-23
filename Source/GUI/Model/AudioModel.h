628b89e73165bccca812c845a0a9e5236f37ce08
xxxDKGxxx <dominikzielinski678@gmail.com>
2026-03-23 13:19:41 +0100

    Initial optimalization

--- Source/GUI/Model/AudioModel.h
+++ Source/GUI/Model/AudioModel.h
@@ -17,10 +17,23 @@ public:
   juce::int64 getLengthInSamples() const;
   double getLengthInSeconds() const;
 
+  struct Peak {
+    float min;
+    float max;
+  };
+
+  const std::vector<float> &getVisualWaveform() const;
+  const std::vector<float> &getVisualTime() const;
+
 private:
   std::unique_ptr<juce::AudioBuffer<float>> audioBuffer;
   double sampleRate;
   unsigned int bitsPerSample;
   unsigned int numChannels;
   juce::int64 lengthInSamples;
+
+  std::vector<float> visualWaveform;
+  std::vector<float> visualTime;
+
+  void calculateVisualWaveform();
 };
